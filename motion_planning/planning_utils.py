cabbc672aca97cd94355d027ded52a1c572be15b
fierval <fierval@gmail.com>
2018-04-14 19:56:15 -0700

    initial commit for motion planning

---
+++ motion_planning/planning_utils.py
@@ -0,0 +1,430 @@
+from enum import Enum
+from queue import PriorityQueue
+import numpy as np
+import matplotlib.pylab as plt
+import networkx as nx
+import numpy.linalg as LA
+
+from shapely.geometry import Polygon, Point, LineString
+from queue import PriorityQueue
+
+def create_grid(data, drone_altitude, safety_distance):
+    """
+    Returns a grid representation of a 2D configuration space
+    based on given obstacle data, drone altitude and safety distance
+    arguments.
+    """
+
+    # minimum and maximum north coordinates
+    north_min = np.floor(np.min(data[:, 0] - data[:, 3]))
+    north_max = np.ceil(np.max(data[:, 0] + data[:, 3]))
+
+    # minimum and maximum east coordinates
+    east_min = np.floor(np.min(data[:, 1] - data[:, 4]))
+    east_max = np.ceil(np.max(data[:, 1] + data[:, 4]))
+
+    # given the minimum and maximum coordinates we can
+    # calculate the size of the grid.
+    north_size = int(np.ceil(north_max - north_min))
+    east_size = int(np.ceil(east_max - east_min))
+
+    # Initialize an empty grid
+    grid = np.zeros((north_size, east_size))
+
+    # Populate the grid with obstacles
+    for i in range(data.shape[0]):
+        north, east, alt, d_north, d_east, d_alt = data[i, :]
+        if alt + d_alt + safety_distance > drone_altitude:
+            obstacle = [
+                int(np.clip(north - d_north - safety_distance - north_min, 0, north_size-1)),
+                int(np.clip(north + d_north + safety_distance - north_min, 0, north_size-1)),
+                int(np.clip(east - d_east - safety_distance - east_min, 0, east_size-1)),
+                int(np.clip(east + d_east + safety_distance - east_min, 0, east_size-1)),
+            ]
+            grid[obstacle[0]:obstacle[1]+1, obstacle[2]:obstacle[3]+1] = 1
+
+    return grid, int(north_min), int(east_min)
+
+
+# Assume all actions cost the same.
+class Action(Enum):
+    """
+    An action is represented by a 3 element tuple.
+
+    The first 2 values are the delta of the action relative
+    to the current grid position. The third and final value
+    is the cost of performing the action.
+    """
+
+    WEST = (0, -1, 1)
+    EAST = (0, 1, 1)
+    NORTH = (-1, 0, 1)
+    SOUTH = (1, 0, 1)
+
+    @property
+    def cost(self):
+        return self.value[2]
+
+    @property
+    def delta(self):
+        return (self.value[0], self.value[1])
+
+
+def valid_actions(grid, current_node):
+    """
+    Returns a list of valid actions given a grid and current node.
+    """
+    valid_actions = list(Action)
+    n, m = grid.shape[0] - 1, grid.shape[1] - 1
+    x, y = current_node
+
+    # check if the node is off the grid or
+    # it's an obstacle
+
+    if x - 1 < 0 or grid[x - 1, y] == 1:
+        valid_actions.remove(Action.NORTH)
+    if x + 1 > n or grid[x + 1, y] == 1:
+        valid_actions.remove(Action.SOUTH)
+    if y - 1 < 0 or grid[x, y - 1] == 1:
+        valid_actions.remove(Action.WEST)
+    if y + 1 > m or grid[x, y + 1] == 1:
+        valid_actions.remove(Action.EAST)
+
+    return valid_actions
+
+
+def a_star(grid, h, start, goal):
+
+    path = []
+    path_cost = 0
+    queue = PriorityQueue()
+    queue.put((0, start))
+    visited = set(start)
+
+    branch = {}
+    found = False
+
+    while not queue.empty():
+        item = queue.get()
+        current_node = item[1]
+        if current_node == start:
+            current_cost = 0.0
+        else:
+            current_cost = branch[current_node][0]
+
+        if current_node == goal:
+            print('Found a path.')
+            found = True
+            break
+        else:
+            for action in valid_actions(grid, current_node):
+                # get the tuple representation
+                da = action.delta
+                next_node = (current_node[0] + da[0], current_node[1] + da[1])
+                branch_cost = current_cost + action.cost
+                queue_cost = branch_cost + h(next_node, goal)
+
+                if next_node not in visited:
+                    visited.add(next_node)
+                    branch[next_node] = (branch_cost, current_node, action)
+                    queue.put((queue_cost, next_node))
+
+    if found:
+        # retrace steps
+        n = goal
+        path_cost = branch[n][0]
+        path.append(goal)
+        while branch[n][1] != start:
+            path.append(branch[n][1])
+            n = branch[n][1]
+        path.append(branch[n][1])
+    else:
+        print('**********************')
+        print('Failed to find a path!')
+        print('**********************')
+    return path[::-1], path_cost
+
+
+
+def heuristic(position, goal_position):
+    return np.linalg.norm(np.array(position) - np.array(goal_position))
+
+def create_voxmap(data, voxel_size=5):
+    """
+    Returns a grid representation of a 3D configuration space
+    based on given obstacle data.
+
+    The `voxel_size` argument sets the resolution of the voxel map.
+    """
+    # minimum and maximum north coordinates
+    north_min = np.floor(np.amin(data[:, 0] - data[:, 3]))
+    north_max = np.ceil(np.amax(data[:, 0] + data[:, 3]))
+
+    # minimum and maximum east coordinates
+    east_min = np.floor(np.amin(data[:, 1] - data[:, 4]))
+    east_max = np.ceil(np.amax(data[:, 1] + data[:, 4]))
+
+    # maximum altitude
+    alt_max = np.ceil(np.amax(data[:, 2] + data[:, 5]))
+
+    # given the minimum and maximum coordinates we can
+    # calculate the size of the grid.
+    north_size = int(np.ceil(north_max - north_min)) // voxel_size
+    east_size = int(np.ceil(east_max - east_min)) // voxel_size
+    alt_size = int(alt_max) // voxel_size
+
+    # Create an empty grid
+    voxmap = np.zeros((north_size, east_size, alt_size), dtype=np.bool)
+
+    for i in range(data.shape[0]):
+        # TODO: fill in the voxels that are part of an obstacle with `True`
+        #
+        # i.e. grid[0:5, 20:26, 2:7] = True
+        north, east, alt, d_north, d_east, d_alt = data[i, :]
+        obstacle = [
+            int(north - d_north - north_min) // voxel_size,
+            int(north + d_north - north_min) // voxel_size,
+            int(east - d_east - east_min) // voxel_size,
+            int(east + d_east - east_min) // voxel_size,
+        ]
+
+        height = int(alt + d_alt) // voxel_size
+        voxmap[obstacle[0]:obstacle[1], obstacle[2]:obstacle[3], 0:height] = True
+
+    return voxmap
+
+
+def can_connect(n1, n2):
+    l = LineString([n1, n2])
+    for p in polygons:
+        if p.crosses(l) and p.height >= min(n1[2], n2[2]):
+            return False
+    return True
+
+def create_graph(nodes, k):
+    g = nx.Graph()
+    tree = KDTree(nodes)
+    for n1 in nodes:
+        # for each node connect try to connect to k nearest nodes
+        idxs = tree.query([n1], k, return_distance=False)[0]
+
+        for idx in idxs:
+            n2 = nodes[idx]
+            if n2 == n1:
+                continue
+
+            if can_connect(n1, n2):
+                g.add_edge(n1, n2, weight=1)
+    return g
+
+
+def a_star_graph(graph, heuristic, start, goal):
+    """Modified A* to work with NetworkX graphs."""
+
+    # TODO: complete
+
+    path = []
+    queue = PriorityQueue()
+    queue.put((0, start))
+    visited = set(start)
+
+    branch = {}
+    found = False
+
+    while not queue.empty():
+        item = queue.get()
+        current_cost = item[0]
+        current_node = item[1]
+
+        if current_node == goal:
+            print('Found a path.')
+            found = True
+            break
+        else:
+            for next_node in graph[current_node]:
+                cost = graph.edges[current_node, next_node]['weight']
+                new_cost = current_cost + cost + heuristic(next_node, goal)
+
+                if next_node not in visited:
+                    visited.add(next_node)
+                    queue.put((new_cost, next_node))
+
+                    branch[next_node] = (new_cost, current_node)
+
+    path = []
+    path_cost = 0
+    if found:
+
+        # retrace steps
+        path = []
+        n = goal
+        path_cost = branch[n][0]
+        while branch[n][1] != start:
+            path.append(branch[n][1])
+            n = branch[n][1]
+        path.append(branch[n][1])
+
+    return path[::-1], path_cost
+
+
+def visualize_graph(grid):
+    fig = plt.figure()
+
+    plt.imshow(grid, cmap='Greys', origin='lower')
+
+    nmin = np.min(data[:, 0])
+    emin = np.min(data[:, 1])
+
+    # draw edges
+    for (n1, n2) in g.edges:
+        plt.plot([n1[1] - emin, n2[1] - emin], [n1[0] - nmin, n2[0] - nmin], 'black' , alpha=0.5)
+
+    # draw all nodes
+    for n1 in nodes:
+        plt.scatter(n1[1] - emin, n1[0] - nmin, c='blue')
+
+    # draw connected nodes
+    for n1 in g.nodes:
+        plt.scatter(n1[1] - emin, n1[0] - nmin, c='red')
+
+    plt.xlabel('NORTH')
+    plt.ylabel('EAST')
+
+    plt.show()
+
+def visualize_path(grid, data, path):
+    '''
+    Visualize path through the graph
+    '''
+    fig = plt.figure()
+
+    plt.imshow(grid, cmap='Greys', origin='lower')
+
+    # Add code to visualize path here
+    nmin = np.min(data[:, 0])
+    emin = np.min(data[:, 1])
+
+    # draw nodes
+    for n1 in g.nodes:
+        plt.scatter(n1[1] - emin, n1[0] - nmin, c='red')
+
+    # draw edges
+    for (n1, n2) in g.edges:
+        plt.plot([n1[1] - emin, n2[1] - emin], [n1[0] - nmin, n2[0] - nmin], 'black')
+
+    # TODO: add code to visualize the path
+    path_pairs = zip(path[:-1], path[1:])
+    for (n1, n2) in path_pairs:
+        plt.plot([n1[1] - emin, n2[1] - emin], [n1[0] - nmin, n2[0] - nmin], 'green')
+
+    plt.xlabel('NORTH')
+    plt.ylabel('EAST')
+
+    plt.show()
+
+def visualize_points(grid, data, points):
+    fig = plt.figure()
+
+    plt.imshow(grid, cmap='Greys', origin='lower')
+
+    nmin = np.min(data[:, 0])
+    emin = np.min(data[:, 1])
+
+    # draw points
+    all_pts = np.array(points)
+    north_vals = all_pts[:,0]
+    east_vals = all_pts[:,1]
+    plt.scatter(east_vals - emin, north_vals - nmin, c='red')
+
+    plt.ylabel('NORTH')
+    plt.xlabel('EAST')
+
+    plt.show()
+
+# Define a simple function to add a z coordinate of 1
+def point(p):
+    return np.array([p[0], p[1], 1.])
+
+def collinearity_float(p1, p2, p3, epsilon=1e-6):
+    collinear = False
+    # TODO: Create the matrix out of three points
+    # Add points as rows in a matrix
+    mat = np.vstack((point(p1), point(p2), point(p3)))
+    # TODO: Calculate the determinant of the matrix.
+    det = np.linalg.det(mat)
+    # TODO: Set collinear to True if the determinant is less than epsilon
+    if det < epsilon:
+        collinear = True
+
+    return collinear
+
+def extract_polygons(data):
+
+    polygons = []
+    for i in range(data.shape[0]):
+        north, east, alt, d_north, d_east, d_alt = data[i, :]
+
+        # TODO: Extract the 4 corners of each obstacle
+        #
+        # NOTE: The order of the points needs to be counterclockwise
+        # in order to work with the simple angle test
+        # Also, `shapely` draws sequentially from point to point.
+        #
+        # If the area of the polygon in shapely is 0
+        # you've likely got a weird order.
+        obstacle = [north - d_north, north + d_north, east - d_east, east + d_east]
+        corners = [(obstacle[0], obstacle[2]), (obstacle[0], obstacle[3]), (obstacle[1], obstacle[3]), (obstacle[1], obstacle[2])]
+
+        # TODO: Compute the height of the polygon
+        height = alt + d_alt
+
+        p = Polygon(corners)
+        polygons.append((p, height))
+
+    return polygons
+
+class Sampler:
+
+    def __init__(self, data):
+        self._polygons = extract_polygons(data)
+        self._xmin = np.min(data[:, 0] - data[:, 3])
+        self._xmax = np.max(data[:, 0] + data[:, 3])
+
+        self._ymin = np.min(data[:, 1] - data[:, 4])
+        self._ymax = np.max(data[:, 1] + data[:, 4])
+
+        self._zmin = 0
+        # limit z-axis
+        self._zmax = 20
+        # Record maximum polygon dimension in the xy plane
+        # multiply by 2 since given sizes are half widths
+        # This is still rather clunky but will allow us to
+        # cut down the number of polygons we compare with by a lot.
+        self._max_poly_xy = 2 * np.max((data[:, 3], data[:, 4]))
+        centers = np.array([p.center for p in self._polygons])
+        self._tree = KDTree(centers, metric='euclidean')
+
+    def sample(self, num_samples):
+        """Implemented with a k-d tree for efficiency."""
+        xvals = np.random.uniform(self._xmin, self._xmax, num_samples)
+        yvals = np.random.uniform(self._ymin, self._ymax, num_samples)
+        zvals = np.random.uniform(self._zmin, self._zmax, num_samples)
+        samples = list(zip(xvals, yvals, zvals))
+
+        pts = []
+        for s in samples:
+            in_collision = False
+            idxs = list(self._tree.query_radius(np.array([s[0], s[1]]).reshape(1, -1), r=self._max_poly_xy)[0])
+            if len(idxs) > 0:
+                for ind in idxs:
+                    p = self._polygons[int(ind)]
+                    if p.contains(s) and p.height >= s[2]:
+                        in_collision = True
+            if not in_collision:
+                pts.append(s)
+
+        return pts
+
+    @property
+    def polygons(self):
+        return self._polygons
