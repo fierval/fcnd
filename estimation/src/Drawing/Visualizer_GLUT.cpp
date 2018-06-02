936adabe534ab80881efa45cdb27024e96af0d08
Boris <borisk@nealanalytics.com>
2018-06-02 10:56:14 -0700

    initial window size

--- estimation/src/Drawing/Visualizer_GLUT.cpp
+++ estimation/src/Drawing/Visualizer_GLUT.cpp
@@ -181,8 +181,8 @@ void Visualizer_GLUT::Update(float simTime)
 void Visualizer_GLUT::initializeGL(int *argcp, char **argv)
 {
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-  glutInitWindowPosition(200, 200);
-  glutInitWindowSize(800, 600);
+  glutInitWindowPosition(300, 50);
+  glutInitWindowSize(1600, 1300);
   _glutWindowNum = glutCreateWindow("Simulator!");
 
   glutMotionFunc(&_g_OnMouseMove);
